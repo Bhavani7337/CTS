2b910b5b11828f365bd8faaed3a76af316abbc6c
Bhavani Darani <daranibavani@gmail.com>
2026-06-16 19:15:55 +0530

    Add Exercise 1 Inventory Management System

---
+++ DEEPSKILLING/Module2-DataStructuresAndAlgorithms/Exercise1-InventoryManagementSystem/InventoryTest.java
@@ -0,0 +1,23 @@
+public class InventoryTest {
+
+    public static void main(String[] args) {
+
+        InventoryManager manager = new InventoryManager();
+
+        manager.addProduct(new Product(101, "Laptop", 10, 55000));
+        manager.addProduct(new Product(102, "Mouse", 50, 500));
+
+        System.out.println("\nInventory:");
+        manager.displayProducts();
+
+        manager.updateProduct(101, 15, 53000);
+
+        System.out.println("\nAfter Update:");
+        manager.displayProducts();
+
+        manager.deleteProduct(102);
+
+        System.out.println("\nAfter Deletion:");
+        manager.displayProducts();
+    }
+}
